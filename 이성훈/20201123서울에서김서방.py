47493577fbdc80970a7cc3b5cf7050e1314ab25d
lsh8889 <65858119+lsh8889@users.noreply.github.com>
2020-11-23 01:27:00 +0900

    Create 20201123서울에서김서방.py

---
+++ 이성훈/20201123서울에서김서방.py
@@ -0,0 +1,10 @@
+# 서울에서김서방찾기 -> https://programmers.co.kr/learn/courses/30/lessons/12919?language=python3
+
+def solution(seoul):
+    for i,v in enumerate(seoul):   #i는 number, v는 값을 뽑아낸다.
+        if(v == 'Kim'):
+            answer='김서방은 '+str(i)+'에 있다'  # str을 안쓰면 문자열로 인신되어 오류나더라 
+    return answer
+
+
+seoul = ['Kim', 'Jane']
